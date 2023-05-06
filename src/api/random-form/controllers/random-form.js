97ee7f4ab41b8a191657a38fdb678ccb915e20ba
kiquecarlos <kiquecarlos@gmail.com>
2023-05-06 20:45:12 -0300

    Finalizado endpoint Gerador de Questionario Randomico

--- src/api/random-form/controllers/random-form.js
+++ src/api/random-form/controllers/random-form.js
@@ -4,28 +4,54 @@
  * A set of functions called "actions" for `random-form`
  */
 
+async function findRandomQuestionsByCategoryId(categoryId) {
+  const QTY_QUESTIONS_PER_CATEGORY = 5
+
+  const questionsIds = (
+    await strapi.db.connection.raw(
+      `
+      SELECT 
+          questions.id AS id
+      FROM questions
+        JOIN questions_category_links ON questions_category_links.question_id = questions.id
+        JOIN options_question_links ON options_question_links.question_id = questions.id
+      WHERE questions_category_links.category_id = ?
+      ORDER BY RANDOM()
+      LIMIT ?;
+      `, [categoryId, QTY_QUESTIONS_PER_CATEGORY]
+    )
+  ).map(question => question.id)
+
+  const questions = await strapi.entityService.findMany(
+    "api::question.question",
+    {
+      filters: {
+        id: { $in: questionsIds }
+      },
+      populate: ['options', 'category']
+    }
+  );
+
+  return questions
+}
+
 module.exports = {
   list: async (ctx, next) => {
     try {
-      //TODO implementar a busca Random
-      const entries = await strapi.entityService.findMany(
-        "api::question.question",
+      let questions = []
+
+      const categories = await strapi.entityService.findMany(
+        "api::category.category",
         {
-          // fields: ["id", "text"],
-          // fields: ["id", "title", "slug", "createdAt"],
-          populate: ['options']
-          // {
-          //   option: true
-          //   // category: {
-          //   //   fields: ["name"],
-          //   // },
-          // },
+          fields: ['id']
         }
-      );
+      )
 
-      console.log(entries)
+      for (const category of categories) {
+        questions = questions.concat(await findRandomQuestionsByCategoryId(category.id))
+      }
 
-      ctx.body = entries;
+      ctx.body = questions
     } catch (err) {
       ctx.body = err;
     }
