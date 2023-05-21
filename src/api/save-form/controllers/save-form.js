a6aebdf2f974d9d9da3f8be74b9002c6c8a3a162
kiquecarlos <kiquecarlos@gmail.com>
2023-05-21 18:36:10 -0300

    criado validação para somente aceitar option que pertence a question

--- src/api/save-form/controllers/save-form.js
+++ src/api/save-form/controllers/save-form.js
@@ -4,13 +4,51 @@
  * A set of functions called "actions" for `save-form`
  */
 
+async function isValidAnswers(answers) {
+
+  if (!answers.length) {
+    return false
+  }
+  let query = `
+            SELECT 
+                COUNT(options_question_links.id) AS qty
+            FROM options_question_links
+            WHERE
+            `
+
+  let queryData = []
+
+  for (const [index, answer] of answers.entries()) {
+
+    if (index !== 0) {
+      query += 'OR '
+    }
+
+    query += '(option_id = ? AND question_id = ?) '
+    queryData = [...queryData, answer.option, answer.question]
+  }
+
+  query += ';'
+
+  const [{ qty }] = (await strapi.db.connection.raw(
+    query, queryData
+  )).rows
+
+  return Number(qty) === answers.length
+}
+
+
+
 module.exports = {
   create: async (ctx, next) => {
     try {
       const { body: answersInput } = ctx.request
 
-      const now = new Date()
+      if (!(await isValidAnswers(answersInput))) {
+        return ctx.badRequest('option not found inside question')
+      }
 
+      const now = new Date()
 
       const form = await strapi.entityService.create('api::form.form',
         {
